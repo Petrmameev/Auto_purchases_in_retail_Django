93087357c3952f182b5060b4a0bd96d5d59dda41
Petrmameev <petr_mameev@mail.ru>
2023-10-22 17:42:20 +0700

    sqlLite

--- netology_pd_diplom/settings.py
+++ netology_pd_diplom/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = "netology_pd_diplom.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
-        "NAME": os.getenv("DB_NAME"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # "default": {
+    #     "ENGINE": os.getenv("DB_ENGINE"),
+    #     "NAME": os.getenv("DB_NAME"),
+    #     "HOST": os.getenv("DB_HOST"),
+    #     "PORT": os.getenv("DB_PORT"),
+    #     "USER": os.getenv("DB_USER"),
+    #     "PASSWORD": os.getenv("DB_PASSWORD"),
+    # }
 }
 
 # Password validation
