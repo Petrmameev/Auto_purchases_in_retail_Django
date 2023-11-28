f3f3eab6e84a9edde511ad6f15f575955f8810de
Petrmameev <petr_mameev@mail.ru>
2023-11-28 21:06:56 +0700

    add new price

--- netology_pd_diplom/settings.py
+++ netology_pd_diplom/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
-CSRF_TRUSTED_ORIGINS = ["http://*.http://localhost/admin/login/"]
+CSRF_TRUSTED_ORIGINS = ["https://*.https://localhost/admin/login/", "https://*.127.0.0.1"]
 
 
 INSTALLED_APPS = [
@@ -141,7 +141,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL")
 SERVER_EMAIL = EMAIL_HOST_USER
